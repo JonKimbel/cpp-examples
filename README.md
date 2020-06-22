c545a69685e76891d8b42beecd125084e5c82aa4
Jon Kimbel <helpimtrappedinanemailaddress@gmail.com>
2020-06-21 20:52:20 -0700

    Add pointer example.

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ Example C++ code for my reference.
 
 ## TODO
 
-*   Go through [the codecademy C++ syllabus](https://www.codecademy.com/learn/learn-c-plus-plus)
-    and add those basics to `basics/core-concepts.cc`.
 *   Look through [C++98](https://en.wikipedia.org/wiki/C%2B%2B#Language) and add example usage to
     `//cpp98/`.
 *   Look through [C++03](https://en.cppreference.com/w/cpp/11) and add example usage to `//cpp11/`.
@@ -13,4 +11,3 @@ Example C++ code for my reference.
 *   Look through [C++14](https://en.cppreference.com/w/cpp/11) and add example usage to `//cpp14/`.
 *   Look through [C++17](https://en.cppreference.com/w/cpp/11) and add example usage to `//cpp17/`.
 *   Look through [C++20](https://en.cppreference.com/w/cpp/11) and add example usage to `//cpp20/`.
-
---
+++ basics/pointers.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include "fmt/format.h"
+
+int** make_matrix_with_pointers();
+void print_matrix_with_pointers(int** matrix);
+void unmake_matrix(int** matrix);
+void pass_by_reference(int& number);
+void pass_by_value(int number);
+void pass_by_pointer(int* number);
+
+int main() {
+  int** matrix = make_matrix_with_pointers();
+  print_matrix_with_pointers(matrix);
+  unmake_matrix(matrix);
+
+  int number = 0;
+  pass_by_value(number);
+  std::cout << fmt::format("Passed by value: {}", number) << std::endl;
+
+  pass_by_reference(number);
+  std::cout << fmt::format("Passed by reference: {}", number) << std::endl;
+
+  pass_by_pointer(&number);
+  std::cout << fmt::format("Passed by pointer: {}", number) << std::endl;
+
+  return 0;
+}
+
+int** make_matrix_with_pointers() {
+  int** matrix;
+
+  matrix = new int*[10];
+
+  for (int x = 0; x < 10; x++) {
+    matrix[x] = new int[10];
+    for (int y = 0; y < 10; y++) {
+      matrix[x][y] = x*10+y;
+    }
+  }
+
+  return matrix;
+}
+
+void print_matrix_with_pointers(int** matrix) {
+  for (int x = 0; x < 10; x++) {
+    for (int y = 0; y < 10; y++) {
+      std::cout << matrix[x][y] << "\t";
+    }
+    std::cout << std::endl;
+  }
+}
+
+void unmake_matrix(int** matrix) {
+  for (int x = 0; x < 10; x++) {
+    delete matrix[x];
+  }
+  delete matrix;
+}
+
+
+void  pass_by_value(int number) {
+  number = 42;
+}
+
+void pass_by_reference(int& number) {
+  number = 42;
+}
+
+void pass_by_pointer(int* number) {
+  *number = 24;
+}
